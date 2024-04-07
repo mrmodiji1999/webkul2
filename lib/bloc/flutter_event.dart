1742ac774ea7f58167692b2cabaf030d0b881691
mrmodiji1999 <rajshing2019@gmail.com>
2024-04-08 01:27:22 +0530

    first commit

---
+++ lib/bloc/flutter_event.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+ abstract class FlutterEvent extends Equatable{
+  @override
+  List<Object?> get props =>[];
+
+}
+class PostFetchEvent extends FlutterEvent{int value;
+PostFetchEvent({required this.value});}
+
+class ViewEvent extends FlutterEvent{}
+class ScaffoldEvent extends FlutterEvent{}
+class ApiDataSendEvent extends FlutterEvent{String email, passwd;
+
+ApiDataSendEvent({required this.email,required this.passwd});
+}
---
+++ lib/repo/posts_service.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'posts_service.g.dart';
+
+@RestApi(baseUrl: "https://api.escuelajs.co/api/v1/products/")
+abstract class PostsService {
+  factory PostsService(Dio dio) = _PostsService;
+   @GET("/")
+  Future<dynamic> fetchProducts({
+    @Query('categoryId') int? categoryId,}
+  );
+  // @GET("/posts")
+  // Future<dynamic> fetchPosts();
+    @POST("/products")
+  Future<dynamic> addPost(@Body() Map<String, dynamic> postData);
+
+  
+}
